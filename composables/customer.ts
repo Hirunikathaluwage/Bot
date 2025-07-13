cde07e1b59aea4f4b18e464a1a28a0e89d91adc9
Hiruni <kathaluwagehiruni@gmail.com>
2025-07-13 20:32:51 +0530

    half working

---
+++ composables/customer.ts
@@ -0,0 +1,7 @@
+export const useCustomer =() => {
+    const customerName= useState("customerName", () => "");
+    const hasNameError = computed(() => customerName.value.length < 2);
+
+    return {customerName,hasNameError };
+
+};
